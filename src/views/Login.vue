137ebd02d4b25c86be1dfa8afc3520f6665be08b
SomiaWhiteRing <55022033+SomiaWhiteRing@users.noreply.github.com>
2021-08-21 21:17:20 +0800

    完成登录模块

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,14 +2,18 @@
 <template>
   <form v-if="!inReg">
     <div class='my_page'>
+      密码:<input type="password" v-model="password"/>
       <button type="button" @click="login()">登录</button>
-      <button type="button" @click="goReg()">注册</button>
+      <button type="button" @click="goReg()">修改密码</button>
     </div>
   </form>
   <form v-else>
     <div class='my_page'>
+      旧密码:<input type="text" v-model="password"/>
+      新密码:<input type="password" v-model="NewPass"/>
+      确认密码:<input type="password" v-model="rePass"/>
       <button type="button" @click="reg()">注册</button>
-      <button type="button" @click="back()">注册</button>
+      <button type="button" @click="back()">返回</button>
     </div>
   </form>
 </template>
@@ -24,7 +28,10 @@ export default {
   data () {
     // 这里存放数据
     return {
-      inReg: false
+      inReg: false,
+      password: '',
+      NewPass: '',
+      rePass: ''
     }
   },
   // 监听属性 类似于data概念
@@ -55,7 +62,30 @@ export default {
       this.inReg = false
     },
     login () {
-      this.$router.push('/home/list')
+      if (localStorage.getItem('password') === null) {
+        alert('您还未设置密码!请前往设置密码界面进行初次设置.')
+        this.inReg = true
+        return
+      }
+      if (this.password === localStorage.getItem('password')) {
+        this.$router.push('/home/list')
+      } else {
+        alert('密码错误!')
+      }
+    },
+    reg () {
+      if (this.NewPass === this.rePass) {
+        if ((localStorage.getItem('password') === null) || (localStorage.getItem('password') === this.password)) {
+          alert('新密码设置成功!现在返回登录界面.')
+          localStorage.setItem('password', this.NewPass)
+          this.password = this.NewPass
+          this.inReg = false
+        } else {
+          alert('旧密码不正确!')
+        }
+      } else {
+        alert('两次密码输入不相同!')
+      }
     }
   }
 }
