68a27c82040afce757e0e60cbc07f79029db13f0
SomiaWhiteRing <55022033+SomiaWhiteRing@users.noreply.github.com>
2021-08-23 11:16:51 +0800

    在初次登陆时追加欢迎文档。

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -37,8 +37,9 @@
 <script>
 // 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 // 例如：import 《组件名称》 from '《组件路径》'
-
+import store from '../store/index.js'
 export default {
+  store,
   // import引入的组件需要注入到对象中才能使用
   components: {},
   data () {
@@ -102,6 +103,11 @@ export default {
             title: '密码设置成功!',
             message: '现在返回登陆界面',
             type: 'success'
+          })
+          store.commit('addItem', {
+            title: '欢迎来到轻记事！',
+            content: '您可以在这里自由记录您的想法而不受任何第三方服务的监控。',
+            date: this.getdate()
           })
           localStorage.setItem('password', this.NewPass)
           this.password = this.NewPass
